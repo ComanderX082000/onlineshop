9bde0618bd5de1acd7e2a0ded0ae82588c8f6a97
ComanderX082000 <shoxruxkalasboyev080808@gmail.com>
2022-04-19 22:26:54 +0300

    shoxrux

--- shop/media/models.py
+++ shop/media/models.py
@@ -1,7 +1,88 @@
+from tabnanny import verbose
 from django.db import models
+from datetime import datetime,timezone
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    class Meta:
+        verbose_name="Kategoriya"
+        verbose_name_plural="Kategoryalar"
+    name = models.CharField(max_length=256,verbose_name="Nomi")
+    icon = models.ImageField(upload_to="images/",verbose_name="Rasm")
+    slug = models.CharField(max_length=256,null=True)
 
-class User(models.Model):
-    name=models.CharField(max_length=255)
-    email=models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    class Meta:
+        verbose_name="Kichik Kategorya"
+        verbose_name_plural="Kichik Kategoryalar"
+
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=256,null=True)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+class ProductColor(models.Model):
+    name = models.CharField(max_length=256)
+
+
+    def __str__(self):
+        return self.name
+
+class ProductSize(models.Model):
+    name = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    class Meta:
+        verbose_name="Mahsulot"
+        verbose_name_plural="Mahsulotlar"
+    
+
+    title = models.CharField(max_length=256)
+    price = models.FloatField()
+    slug = models.CharField(max_length=256,null=True)
+    description = models.TextField()
+    rating = models.FloatField()
+    image = models.ImageField(upload_to="images/")
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    color = models.ManyToManyField(ProductColor)
+    size = models.ManyToManyField(ProductSize)
+    is_active = models.BooleanField(default=True)
+
+    def get_rating_percent(self):
+        return 100 * (self.rating / 5)
+    
+
+    def is_new_product(self):
+        time_delta = datetime.now(timezone.utc) - self.created_at
+        return time_delta.seconds < settings.NEW_PRODUCT_SECONDS
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="images/", )
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+
+
+
+        
+
+
+
+
+
+
+
+
+
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-m39h#v$0eent-s4y2+bkh3%+wg8pm7n&af4$#z*mt-$^nev8_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'media'
+    'media',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,9 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+NEW_PRODUCT_SECONDS = 86400
