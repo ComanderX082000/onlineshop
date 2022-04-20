0f06585a0617d1b32bed698ac05fb6f805ffef07
ComanderX082000 <shoxruxkalasboyev080808@gmail.com>
2022-04-20 19:15:44 +0300

    shoxrux

--- shop/media/admin.py
+++ shop/media/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","price","description","rating"]
+    list_display_links =["title","price"] 
+    search_fields = ["price","title","rating"]
+
+   
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug":("name",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields={"slug":("name",)}
+
+
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(SubCategory,SubCategoryAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(ProductColor)
+admin.site.register(Productsize)
--- shop/media/models.py
+++ shop/media/models.py
@@ -2,6 +2,8 @@ from tabnanny import verbose
 from django.db import models
 from datetime import datetime,timezone
 from django.conf import settings
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     class Meta:
@@ -11,24 +13,31 @@ class Category(models.Model):
     icon = models.ImageField(upload_to="images/",verbose_name="Rasm")
     slug = models.CharField(max_length=256,null=True)
 
-
     def __str__(self):
         return self.name
-
 class SubCategory(models.Model):
-    class Meta:
-        verbose_name="Kichik Kategorya"
-        verbose_name_plural="Kichik Kategoryalar"
+    verbose_name='Kichik Kategorya'
+    verbose_name_plural="Kichik Kategoryalar"
 
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name="Nomi")
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
     slug = models.CharField(max_length=256,null=True)
-
-
-
     def __str__(self):
         return self.name
 
+class SubSubCategory(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Nomi")
+    category = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=256,null=True)
+    def __str__(self):
+        return self.name
+
+class Tags(models.Model):
+    name = models.CharField(max_length=256, verbose_name="Nomi")
+    category = models.ForeignKey(SubSubCategory,on_delete=models.CASCADE)
+    slug = models.CharField(max_length=256,null=True)
+    def __str__(self):
+        return self.name
 
 class ProductColor(models.Model):
     name = models.CharField(max_length=256)
@@ -37,12 +46,12 @@ class ProductColor(models.Model):
     def __str__(self):
         return self.name
 
-class ProductSize(models.Model):
+class Productsize(models.Model):
     name = models.CharField(max_length=256)
-
     def __str__(self):
         return self.name
 
+   
 
 class Product(models.Model):
     class Meta:
@@ -52,14 +61,21 @@ class Product(models.Model):
 
     title = models.CharField(max_length=256)
     price = models.FloatField()
-    slug = models.CharField(max_length=256,null=True)
+    slug =models.SlugField(default='',editable=False)
     description = models.TextField()
     rating = models.FloatField()
     image = models.ImageField(upload_to="images/")
-    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
-    color = models.ManyToManyField(ProductColor)
-    size = models.ManyToManyField(ProductSize)
+    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    color = models.ManyToManyField(ProductColor,related_name='products')
+    size = models.ManyToManyField(Productsize,related_name='products')
     is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.price}"
+    
 
     def get_rating_percent(self):
         return 100 * (self.rating / 5)
@@ -68,11 +84,21 @@ class Product(models.Model):
     def is_new_product(self):
         time_delta = datetime.now(timezone.utc) - self.created_at
         return time_delta.seconds < settings.NEW_PRODUCT_SECONDS
+    
+class Variations(models.Model):
+    product_id=models.IntegerField()
+    name=models.CharField(max_length=255)
+    slug=models.CharField(max_length=255)
 
+    def save(self, *args, **kwargs):
+        value = self.title
+        self.slug = slugify(value, allow_unicode=True)
+        super().save(*args, **kwargs)
 
 class ProductImage(models.Model):
     image = models.ImageField(upload_to="images/", )
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="product_images")
+
 
 
 
